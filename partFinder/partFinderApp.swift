4f25492070c0ba5f7485ad7e7e64cd387571179e
gerardo <gece95.ca@gmail.com>
2025-02-05 15:25:16 -0800

    Initial Commit

---
+++ partFinder/partFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  partFinderApp.swift
+//  partFinder
+//
+//  Created by Gerardo Cervantes on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct partFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ partFinderTests/partFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  partFinderTests.swift
+//  partFinderTests
+//
+//  Created by Gerardo Cervantes on 2/5/25.
+//
+
+import Testing
+@testable import partFinder
+
+struct partFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
